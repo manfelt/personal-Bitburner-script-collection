b2887543c76590eadf977181b6c74ed00eb6a584
Magnus Manfelt <35919896+manfelt@users.noreply.github.com>
2024-09-16 13:44:30 +0200

    Update homeMega.js
    Added RAM-less servers

--- homeMega.js
+++ homeMega.js
@@ -9,11 +9,13 @@ export async function main(ns) {
   var srlist = ['run4theh111z', 'n00dles', 'foodnstuff', 'sigma-cosmetics',
     'joesguns', 'hong-fang-tea', 'harakiri-sushi', 'iron-gym', 'zer0', 'max-hardware', 'nectar-net',
     'CSEC', 'neo-net', 'silver-helix', 'phantasy', 'omega-net', 'the-hub',
-    'netlink', 'avmnite-02h', 'titan-labs', 'johnson-ortho',
+    'netlink', 'avmnite-02h', 'titan-labs', 'johnson-ortho', 'computek', 'crush-fitness', 'syscore', 'galactic-cyber', 'snap-fitness', 'aerocorp', 'deltaone', 'univ-energy',
+    'infocomm', 'zeus-med', 'taiyang-digital', 'applied-energetics', 'kuai-gong', 'icarus', 'defcomm', 'zb-def', 'omnitek', '4sigma', 'stormtech',
+    'megacorp', 'ecorp', 'The-Cave', 'nwo', 'fulcrumassets', 'nova-med', 'clarkinc', 'b-and-a', 'powerhouse-fitness',
     'zb-institute', 'catalyst', 'I.I.I.I', 'rothman-uni', 'univ-energy',
     'summit-uni', 'aevum-police', 'rho-construction', 'millenium-fitness',
     'lexo-corp', 'solaris', 'alpha-ent', 'omnia', 'unitalife', 'global-pharm', 'microdyne',
-    'helios', 'vitalife', '.', 'blade', 'fulcrumtech'];
+    'helios', 'vitalife', '.', 'blade', 'fulcrumtech']
 
 
   function getAvailableSrv(srlist) {
